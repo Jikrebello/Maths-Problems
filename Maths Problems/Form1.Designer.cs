170818826c0ee329d53b120d98d1ad58fb5ba410
Jikrebello <33652252+Jikrebello@users.noreply.github.com>
2021-05-19 23:33:20 +0200

    Add files via upload

---
+++ Maths Problems/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿
+namespace Maths_Problems
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnVolumeOfRectangle = new System.Windows.Forms.Button();
+            this.BtnPricePerPound = new System.Windows.Forms.Button();
+            this.BtnCarpetArea = new System.Windows.Forms.Button();
+            this.BtnMarkupCalculator = new System.Windows.Forms.Button();
+            this.BtnCommisionCalculator = new System.Windows.Forms.Button();
+            this.BtnEmployeeTaxCalculator = new System.Windows.Forms.Button();
+            this.BtnWineCalculator = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BtnVolumeOfRectangle
+            // 
+            this.BtnVolumeOfRectangle.Location = new System.Drawing.Point(12, 12);
+            this.BtnVolumeOfRectangle.Name = "BtnVolumeOfRectangle";
+            this.BtnVolumeOfRectangle.Size = new System.Drawing.Size(195, 43);
+            this.BtnVolumeOfRectangle.TabIndex = 0;
+            this.BtnVolumeOfRectangle.Text = "Volume of Rectangle";
+            this.BtnVolumeOfRectangle.UseVisualStyleBackColor = true;
+            this.BtnVolumeOfRectangle.Click += new System.EventHandler(this.BtnVolumeOfRectangle_Click);
+            // 
+            // BtnPricePerPound
+            // 
+            this.BtnPricePerPound.Location = new System.Drawing.Point(12, 61);
+            this.BtnPricePerPound.Name = "BtnPricePerPound";
+            this.BtnPricePerPound.Size = new System.Drawing.Size(195, 43);
+            this.BtnPricePerPound.TabIndex = 1;
+            this.BtnPricePerPound.Text = "Price per Pound";
+            this.BtnPricePerPound.UseVisualStyleBackColor = true;
+            this.BtnPricePerPound.Click += new System.EventHandler(this.BtnPricePerPound_Click);
+            // 
+            // BtnCarpetArea
+            // 
+            this.BtnCarpetArea.Location = new System.Drawing.Point(12, 110);
+            this.BtnCarpetArea.Name = "BtnCarpetArea";
+            this.BtnCarpetArea.Size = new System.Drawing.Size(195, 43);
+            this.BtnCarpetArea.TabIndex = 2;
+            this.BtnCarpetArea.Text = "Carpet Area";
+            this.BtnCarpetArea.UseVisualStyleBackColor = true;
+            this.BtnCarpetArea.Click += new System.EventHandler(this.BtnCarpetArea_Click);
+            // 
+            // BtnMarkupCalculator
+            // 
+            this.BtnMarkupCalculator.Location = new System.Drawing.Point(12, 159);
+            this.BtnMarkupCalculator.Name = "BtnMarkupCalculator";
+            this.BtnMarkupCalculator.Size = new System.Drawing.Size(195, 43);
+            this.BtnMarkupCalculator.TabIndex = 3;
+            this.BtnMarkupCalculator.Text = "Markup Calculator";
+            this.BtnMarkupCalculator.UseVisualStyleBackColor = true;
+            this.BtnMarkupCalculator.Click += new System.EventHandler(this.BtnMarkupCalculator_Click);
+            // 
+            // BtnCommisionCalculator
+            // 
+            this.BtnCommisionCalculator.Location = new System.Drawing.Point(12, 208);
+            this.BtnCommisionCalculator.Name = "BtnCommisionCalculator";
+            this.BtnCommisionCalculator.Size = new System.Drawing.Size(195, 43);
+            this.BtnCommisionCalculator.TabIndex = 4;
+            this.BtnCommisionCalculator.Text = "Commission Calculator";
+            this.BtnCommisionCalculator.UseVisualStyleBackColor = true;
+            this.BtnCommisionCalculator.Click += new System.EventHandler(this.BtnCommisionCalculator_Click);
+            // 
+            // BtnEmployeeTaxCalculator
+            // 
+            this.BtnEmployeeTaxCalculator.Location = new System.Drawing.Point(12, 257);
+            this.BtnEmployeeTaxCalculator.Name = "BtnEmployeeTaxCalculator";
+            this.BtnEmployeeTaxCalculator.Size = new System.Drawing.Size(195, 43);
+            this.BtnEmployeeTaxCalculator.TabIndex = 5;
+            this.BtnEmployeeTaxCalculator.Text = "Calculate Tax";
+            this.BtnEmployeeTaxCalculator.UseVisualStyleBackColor = true;
+            this.BtnEmployeeTaxCalculator.Click += new System.EventHandler(this.BtnEmployeeTaxCalculator_Click);
+            // 
+            // BtnWineCalculator
+            // 
+            this.BtnWineCalculator.Location = new System.Drawing.Point(12, 306);
+            this.BtnWineCalculator.Name = "BtnWineCalculator";
+            this.BtnWineCalculator.Size = new System.Drawing.Size(195, 43);
+            this.BtnWineCalculator.TabIndex = 6;
+            this.BtnWineCalculator.Text = "Wine Calculator";
+            this.BtnWineCalculator.UseVisualStyleBackColor = true;
+            this.BtnWineCalculator.Click += new System.EventHandler(this.BtnWineCalculator_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(221, 363);
+            this.Controls.Add(this.BtnWineCalculator);
+            this.Controls.Add(this.BtnEmployeeTaxCalculator);
+            this.Controls.Add(this.BtnCommisionCalculator);
+            this.Controls.Add(this.BtnMarkupCalculator);
+            this.Controls.Add(this.BtnCarpetArea);
+            this.Controls.Add(this.BtnPricePerPound);
+            this.Controls.Add(this.BtnVolumeOfRectangle);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnVolumeOfRectangle;
+        private System.Windows.Forms.Button BtnPricePerPound;
+        private System.Windows.Forms.Button BtnCarpetArea;
+        private System.Windows.Forms.Button BtnMarkupCalculator;
+        private System.Windows.Forms.Button BtnCommisionCalculator;
+        private System.Windows.Forms.Button BtnEmployeeTaxCalculator;
+        private System.Windows.Forms.Button BtnWineCalculator;
+    }
+}
+
---
+++ Maths Problems/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace Maths_Problems
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        /*
+         *Find the volume of a rectangular solid when given the length,width and height. 
+        */
+        private void BtnVolumeOfRectangle_Click(object sender, EventArgs e)
+        {
+            float length;
+            float width;
+            float height; 
+
+            length = float.Parse(Interaction.InputBox("Enter the Length.", "", "0"), CultureInfo.InvariantCulture.NumberFormat);
+            width  = float.Parse(Interaction.InputBox("Enter the Width.", "", "0"), CultureInfo.InvariantCulture.NumberFormat);
+            height = float.Parse(Interaction.InputBox("Enter the Height.", "", "0"), CultureInfo.InvariantCulture.NumberFormat);
+
+
+            float volume = length * width * height;
+
+            MessageBox.Show("The volume is " + volume);
+        }
+
+        /*
+         * A delicatessen sells cold cuts by the pound. Write a program which calculates the 
+         * customer’s bill after accepting the pounds purchased and price per pound.
+        */
+        private void BtnPricePerPound_Click(object sender, EventArgs e)
+        {
+            float pounds;
+            float price;
+
+            pounds = float.Parse(Interaction.InputBox("Enter the amount of pounds.", "", "0"), CultureInfo.InvariantCulture.NumberFormat);
+            price = float.Parse(Interaction.InputBox("Enter the price per pound.", "", "0"), CultureInfo.InvariantCulture.NumberFormat);
+
+            float bill = pounds * price;
+
+            MessageBox.Show("The total price per pound is " + Math.Round(bill, 2));
+        }
+
+        /*
+         * Find the cost of broadloom given the length and width of a room in metres. The cost of
+         * the broadloom is $12 per square metres.
+        */
+        private void BtnCarpetArea_Click(object sender, EventArgs e)
+        {
+            float length;
+            float width;
+            float pricePerMetre = 12f;
+
+            length = float.Parse(Interaction.InputBox("Enter the length for the room.", "", "0"), CultureInfo.InvariantCulture.NumberFormat);
+            width = float.Parse(Interaction.InputBox("Enter the width for the room.", "", "0"), CultureInfo.InvariantCulture.NumberFormat);
+
+            pricePerMetre = length * width * pricePerMetre;
+
+            MessageBox.Show("The cost of the carpet will be R" + pricePerMetre);
+        }
+
+
+        /*
+         * Write a program to calculate the percentage markup for any store item, given the cost and
+         * the selling price.
+        */
+        private void BtnMarkupCalculator_Click(object sender, EventArgs e)
+        {
+            float costOfItem;
+            float sellingPriceOfItem;
+            float markup;
+
+            costOfItem = float.Parse(Interaction.InputBox("Enter the cost of the item.", "", "0"), CultureInfo.InvariantCulture.NumberFormat);
+            sellingPriceOfItem = float.Parse(Interaction.InputBox("Enter the selling price of the item.", "", "0"), CultureInfo.InvariantCulture.NumberFormat);
+
+            markup = ((sellingPriceOfItem - costOfItem) / costOfItem) * 100;
+
+            MessageBox.Show("The markup of the item is " + markup + "%");
+        }
+
+
+        /*
+         * Write a program which accepts a salespersons sales for one week and then determines 
+         * commission. Commission is 14% of sales.
+        */
+        private void BtnCommisionCalculator_Click(object sender, EventArgs e)
+        {
+            float salesForTheWeek;
+            float commission;
+
+            salesForTheWeek = float.Parse(Interaction.InputBox("Enter the sales for the week.", "", "0"), CultureInfo.InvariantCulture.NumberFormat);
+
+            commission = salesForTheWeek * 0.14f;
+
+            MessageBox.Show("The Commission is R" + Math.Round(commission, 2));
+        }
+
+        /*
+         * Write a program which accepts an employees name, rate of pay and hours worked. The 
+         * following values should then be calculated: Gross pay, Taxes (20% of gross pay), and netpay.
+        */
+        private void BtnEmployeeTaxCalculator_Click(object sender, EventArgs e)
+        {
+            string employeeName;
+            float rateOfPay;
+            float hoursWorked;
+            float grossPay;
+            float taxes;
+            float netPay;
+
+            employeeName = Interaction.InputBox("Enter Employee name.", "", "0");
+            rateOfPay = float.Parse(Interaction.InputBox("Enter the pay rate for the employee.", "", "0"), CultureInfo.InvariantCulture.NumberFormat);
+            hoursWorked = float.Parse(Interaction.InputBox("Enter the amount of hours worked.", "", "0"), CultureInfo.InvariantCulture.NumberFormat);
+
+            grossPay = rateOfPay * hoursWorked;
+            taxes = grossPay * 0.20f;
+            netPay = grossPay - taxes;
+
+            MessageBox.Show("The gross pay for " + employeeName + " is R" + grossPay + ".\n" +
+                            "The taxes is R" + taxes + ".\n" + 
+                            "The net pay is R" + Math.Round(netPay, 2) + ".");
+        }
+
+
+        /*
+         * A vineyard measures the amount of wine in its storage tanks in gallons, but it must ship 
+         * the wine in barrels. There are 159 litres in a barrel. Write a program that will accept an 
+         * input, the number of gallons in the tank, and display using a MessageBox.Show() the 
+         * number of barrels that can be shipped, and the number of gallons remaining after as many 
+         * barrels as possible have been filled.
+         */
+        private void BtnWineCalculator_Click(object sender, EventArgs e)
+        {
+            float wineInLitres;
+            float numberOfBarrels;
+            float barrelCapacity = 159;
+            float filledUpBarrel;
+            float amountOfRemainingLitres;
+
+            wineInLitres = float.Parse(Interaction.InputBox("Enter the amount of wine in litres.", "", "0"), CultureInfo.InvariantCulture.NumberFormat);
+            numberOfBarrels = float.Parse(Interaction.InputBox("Enter the amount of barrels.", "", "0"), CultureInfo.InvariantCulture.NumberFormat);
+
+            filledUpBarrel = (wineInLitres / barrelCapacity) * numberOfBarrels;
+
+            amountOfRemainingLitres = wineInLitres % filledUpBarrel;
+
+            MessageBox.Show(wineInLitres + "ℓ of wine can fill " + numberOfBarrels + " barrel/s with " + Math.Round(amountOfRemainingLitres, 2) + "ℓ of wine remaining.");
+        }
+    }
+}
